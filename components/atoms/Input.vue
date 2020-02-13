dcf357860d15bed10a31dea793be3da6a34e34b2
Hidehiko Naito <hidehiko@7110.email>
2020-02-14 00:47:40 +0900

    add Input

---
+++ components/atoms/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" class="Input" :value="value" @input="$emit('input', $event.target.value)">
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Input {
+  width: 100%;
+  max-width: $pokemon-base * 60;
+  height: $pokemon-base * 10;
+  padding: 0 $pokemon-base;
+  font-size: 14px;
+}
+</style>
